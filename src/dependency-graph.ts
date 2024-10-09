78abebde736a8051fcdcfa46c369738684621b34
Eivin Giske Skaaren <cyh@qlik.com>
2024-10-09 19:56:32 +0200

    Add support for checking all dependencies

--- src/dependency-graph.ts
+++ src/dependency-graph.ts
@@ -39,7 +39,7 @@ export async function compare({
       }
     )
 
-    core.info(`headDependencies: ${JSON.stringify(headDependencies)}`)
+    core.debug(`headDependencies: ${JSON.stringify(headDependencies)}`)
 
     // Parse the dependencies using the existing schema
     const headChanges = ChangesSchema.parse(headDependencies.data)
@@ -59,7 +59,7 @@ export async function compare({
       snapshot_warnings
     })
   } else {
-    core.info('headDependencies: NOOOOOOOOOO!!!!!!')
+    core.debug('NOOOOOOOOOO!!!!!! NOOOOOOO!!!!!')
 
     const changes = await octo.request(
       'GET /repos/{owner}/{repo}/dependency-graph/compare/{basehead}',
